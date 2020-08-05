30fea097774d76fc5cc81eca3e0a82fa51c756f9
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-08-05 17:19:49 -0600

    Add files via upload

---
+++ Chapter 9/testRectangle.java
@@ -0,0 +1,33 @@
+/* Dallin Savage
+ * 8/5/20
+ * create a rectangle
+ */
+
+public class testRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rec1 = new Rectangle(4, 40);
+		Rectangle rec2 = new Rectangle(3.5, 35.9);
+		System.out.println("Rectangle 1: width-" + rec1.width + "  Height-" + rec1.height + "  Area-" + rec1.getArea() + "  Perimeter-" + rec1.getPerimeter());
+		System.out.println("Rectangle 1: width-" + rec2.width + "  Height-" + rec2.height + "  Area-" + rec2.getArea() + "  Perimeter-" + rec2.getPerimeter());
+	}
+
+}
+
+	class Rectangle {
+		double width = 1;
+		double height = 1;
+		Rectangle() {
+		}
+		Rectangle(double newWidth, double newHeight) {
+			width = newWidth;
+			height = newHeight;
+		}
+		double getArea() {
+			return width * height;
+		}
+		double getPerimeter() {
+			return (width * 2) + (height *2);
+		}
+	}
