627f9493a08836d775acd4c4016347a8d40e8883
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-08-10 17:51:19 -0600

    Add files via upload

---
+++ Chapter 10/MyInteger.java
@@ -0,0 +1,69 @@
+
+public class MyInteger {
+	
+	private int value = 0;
+	
+	public static int MyInteger(int newValue) {
+		value = newValue;
+	}
+	public int getValue() {
+		return value;
+	}
+	public boolean isEven() {
+		if (value % 2 == 0) {
+			return true;
+		}
+	}
+	public boolean isOdd() {
+		if (value % 2 != 0) {
+			return true;
+		}
+	}
+	public boolean isPrime() {
+		
+	}
+	public static boolean isEven(int value) {
+		if (value % 2 == 0) {
+			return true;
+		}
+	}
+	public static boolean isOdd(int value) {
+		if (value % 2 != 0) {
+			return true;
+		}
+	}
+	public static boolean isPrime(int value) {
+		
+	}
+	public static boolean isEven(MyInteger value) {
+		if (value % 2 == 0) {
+			return true;
+		}
+	}
+	public static boolean isOdd(MyInteger value) {
+		if (value % 2 != 0) {
+			return true;
+		}
+	}
+	public static boolean isPrime(MyInteger) {
+		
+	}
+	public boolean equals(int) {
+		if ()
+	}
+	public boolean equals(MyInteger) {
+		
+	}
+	public static int parseInt(char[]) {
+		
+	}
+	public static int parseInt(String) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
