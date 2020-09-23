d5c9c988c458988f6a8b5d8082ab23e02eda1729
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-09-23 17:42:12 -0600

    Update Count_down.java

--- Chapter 16/Count_down.java
+++ Chapter 16/Count_down.java
@@ -1,5 +1,8 @@
+/* Dallin Savage
+ * 9/23/20
+ */
 
-import javafx.animation.KeyFrame;
+
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -7,33 +10,35 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
 
 public class Count_down extends Application{
 
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		String MEDIA_URL = "https://liveexample.pearsoncmg.com/common/audio/anthem/anthem0.mp3";
+		Media media = new Media(MEDIA_URL);
+		MediaPlayer sound = new MediaPlayer(media);
 		TextField text = new TextField();
+		Timeline animation = new Timeline();
 		Pane pane = new Pane();
 		pane.getChildren().add(text);
 		text.setAlignment(Pos.CENTER);
 		text.setOnKeyPressed(e -> {
-			e.getCode();
-			if (e.getCode() == KeyCode.ENTER) {
-				while (text.getText() != "0") {
-				Timeline counting = new Timeline(
-					new KeyFrame (Duration.seconds(1)));
-				counting.setCycleCount(1);
-				counting.play();
-				text.setText(countDown(text.getText()));
+			if (e.getCode().equals(KeyCode.ENTER)) {
+				while (!text.getText().equals("0")) {
+						text.setText(countDown(text.getText()));
+						animation.play();
 				}
+				sound.play();
 			}
 		});
 		
@@ -50,5 +55,4 @@ public class Count_down extends Application{
 		String newNum = String.valueOf(intValue);
 		return newNum;
 	}
-
 }
