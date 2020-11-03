1d250fb07b801b2ee21f188cf34553152b012c75
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-11-02 17:15:12 -0700

    Add files via upload

---
+++ Chapter 24/exercise_24_1/MyAbstractList.java
@@ -0,0 +1,83 @@
+public abstract class MyAbstractList<E> implements MyList<E> {
+	protected int size = 0; // The size of the list
+
+	/** Create a default list */
+	protected MyAbstractList() {
+	}
+
+	/** Create a list from an array of objects */
+	protected MyAbstractList(E[] objects) {
+		for (int i = 0; i < objects.length; i++)
+			add(objects[i]);
+	}
+
+	/** Add a new element at the end of this list */
+	public void add(E e) {
+		add(size, e);
+	}
+
+	/** Return true if this list contains no elements */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/** Return the number of elements in this list */
+	public int size() {
+		return size;
+	}
+
+	/** Remove the first occurrence of the element o from this list.
+	 *  Shift any subsequent elements to the left.
+	 *  Return true if the element is removed. */
+	public boolean remove(E e) {
+		if (indexOf(e) >= 0) {
+			remove(indexOf(e));
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	public boolean addAll(MyList<E> otherList) {
+		if (otherList.size() > 0) {
+		for (int i = 0; i < otherList.size(); i++) {
+			add(otherList.get(i));
+		}
+		return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean removeAll(MyList<E> otherList) {
+		boolean changed = false;
+		for (int i = 0; i < otherList.size(); i++) {
+			if (contains(otherList.get(i))) {
+				while (contains(otherList.get(i))) {
+					remove(indexOf(otherList.get(i)));
+				}
+				changed = true;
+			}
+		}
+		return changed;
+	}
+	
+	public boolean retainAll(MyList<E> otherList) {
+		boolean changed = false;
+		for (int i = 0; i < size(); i++) {
+			int counter = 0;
+			for ( int x = 0; x < otherList.size(); x++) {
+				if (get(i).equals(otherList.get(x))) {
+					counter++;
+				}
+			}
+			if (counter == 0) {
+				remove(i);
+				i--;
+				changed = true;
+			}
+	}
+		return changed;
+}
+}
