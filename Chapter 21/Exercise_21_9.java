192231f211066d84d5251cef679f4c20d4ec9268
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2020-11-04 16:44:42 -0700

    Add files via upload

---
+++ Chapter 21/Exercise_21_9.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Exercise_21_9 {
+  public static void main(String[] args) {
+    String[][] stateCapital = {
+      {"Alabama", "Montgomery"},
+      {"Alaska", "Juneau"},
+      {"Arizona", "Phoenix"},
+      {"Arkansas", "Little Rock"},
+      {"California", "Sacramento"},
+      {"Colorado", "Denver"},
+      {"Connecticut", "Hartford"},
+      {"Delaware", "Dover"},
+      {"Florida", "Tallahassee"},
+      {"Georgia", "Atlanta"},
+      {"Hawaii", "Honolulu"},
+      {"Idaho", "Boise"},
+      {"Illinois", "Springfield"},
+      {"Indiana", "Indianapolis"},
+      {"Iowa", "Des Moines"},
+      {"Kansas", "Topeka"},
+      {"Kentucky", "Frankfort"},
+      {"Louisiana", "Baton Rouge"},
+      {"Maine", "Augusta"},
+      {"Maryland", "Annapolis"},
+      {"Massachusettes", "Boston"},
+      {"Michigan", "Lansing"},
+      {"Minnesota", "Saint Paul"},
+      {"Mississippi", "Jackson"},
+      {"Missouri", "Jefferson City"},
+      {"Montana", "Helena"},
+      {"Nebraska", "Lincoln"},
+      {"Nevada", "Carson City"},
+      {"New Hampshire", "Concord"},
+      {"New Jersey", "Trenton"},
+      {"New York", "Albany"},
+      {"New Mexico", "Santa Fe"},
+      {"North Carolina", "Raleigh"},
+      {"North Dakota", "Bismarck"},
+      {"Ohio", "Columbus"},
+      {"Oklahoma", "Oklahoma City"},
+      {"Oregon", "Salem"},
+      {"Pennsylvania", "Harrisburg"},
+      {"Rhode Island", "Providence"},
+      {"South Carolina", "Columbia"},
+      {"South Dakota", "Pierre"},
+      {"Tennessee", "Nashville"},
+      {"Texas", "Austin"},
+      {"Utah", "Salt Lake City"},
+      {"Vermont", "Montpelier"},
+      {"Virginia", "Richmond"},
+      {"Washington", "Olympia"},
+      {"West Virginia", "Charleston"},
+      {"Wisconsin", "Madison"},
+      {"Wyoming", "Cheyenne"}
+    };
+    HashMap states = new HashMap();
+    for (int i = 0; i < stateCapital.length; i++) {
+    	states.put(stateCapital[i][0].toUpperCase(), stateCapital[i][1]);
+    }
+    while (true) {
+    System.out.println("Enter a State");
+    Scanner input = new Scanner(System.in);
+    String capital = input.next().toUpperCase();
+    System.out.println("The Capital of " + capital + " is " + states.get(capital));
+    }
+  }
+}
